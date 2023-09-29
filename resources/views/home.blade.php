15babbda07824d1389f5400e4668bccb57b77deb
ElyPoli <itsos.poli@gmail.com>
2023-09-29 15:50:34 +0200

    created start files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,8 @@
 @extends("layouts.public")
 
+{{-- Indico il titolo della pagina --}}
+@section('title', 'Laravel comics - Home')
+
 @section("content")
     <h1>Hello world</h1>
 @endsection
--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -1,15 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
 
-    @vite(["resources/js/app.js", "resources/scss/app.scss"])
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title> @yield('title') </title>
+
+    {{-- Icona --}}
+    <link rel="icon" href="{{ asset('img/favicon.ico') }}">
+
+    {{-- Inlcudo gli assets con vite --}}
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 </head>
+
 <body>
+    {{-- Includo la navbar --}}
+    @include('partials.header')
     <main>
-        @yield("content")
+        {{-- Segnaposto per il contenuto principale della pagina --}}
+        @yield('content')
     </main>
+    {{-- Includo il footer --}}
+    @include('partials.footer')
 </body>
+
 </html>
