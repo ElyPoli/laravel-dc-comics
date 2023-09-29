9af36efa897c606e8ff5477cbafd20e4736d320e
ElyPoli <itsos.poli@gmail.com>
2023-09-29 17:25:18 +0200

    update controller date for footer and header

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -7,11 +7,113 @@ use App\Models\Comic;
 
 class ComicController extends Controller
 {
-    public function index() {
-        $data = Comic::all();
+    public function index()
+    {
+        $data = [
+            "navLinks" => [
+                [
+                    "name" => "characters",
+                ],
+                [
+                    "name" => "comics",
+                ],
+                [
+                    "name" => "movies",
+                ],
+                [
+                    "name" => "tv",
+                ],
+                [
+                    "name" => "games",
+                ],
+                [
+                    "name" => "collectibles",
+                ],
+                [
+                    "name" => "videos",
+                ],
+                [
+                    "name" => "fans",
+                ],
+                [
+                    "name" => "news",
+                ],
+                [
+                    "name" => "shop",
+                ]
+            ],
+            "footerLinks" => [
+                [
+                    "title" => "DC comics",
+                    "subheading" => [
+                        "Characters",
+                        "Comics",
+                        "Movies",
+                        "TV",
+                        "Games",
+                        "Videos",
+                        "News",
+                    ]
+                ],
+                [
+                    "title" => "Shop",
+                    "subheading" => [
+                        "Shop DC",
+                        "Shop DC collectibles",
+                    ]
+                ],
+                [
+                    "title" => "DC",
+                    "subheading" => [
+                        "Terms Of Use",
+                        "Privacy policy (New)",
+                        "Ad Choices",
+                        "Advertising",
+                        "Jobs",
+                        "Subscriptions",
+                        "Talent Workshops",
+                        "CPSC Certificates",
+                        "Ratings",
+                        "Shop Help",
+                        "Contact Us",
+                    ]
+                ],
+                [
+                    "title" => "Sites",
+                    "subheading" => [
+                        "DC",
+                        "MAD Magazine",
+                        "DC Kids",
+                        "DC Universe",
+                        "DC Power Visa",
+                    ]
+                ]
+            ],
+            "bannerLinks" => [
+                [
+                    "name" => "digital comics",
+                    "srcImg" => "img/buy-comics-digital-comics.png",
+                ],
+                [
+                    "name" => "dc merchandise",
+                    "srcImg" => "img/buy-comics-merchandise.png",
+                ],
+                [
+                    "name" => "subscriptions",
+                    "srcImg" => "img/buy-comics-subscriptions.png",
+                ],
+                [
+                    "name" => "comic shop locator",
+                    "srcImg" => "img/buy-comics-shop-locator.png",
+                ],
+                [
+                    "name" => "dc power visa",
+                    "srcImg" => "img/buy-dc-power-visa.svg",
+                ]
+            ],
+            "comicsList" => Comic::all(),
+        ];
 
-		return view('index', [
-            "comicsList" => $data
-        ]);
+        return view('index', $data);
     }
 }
