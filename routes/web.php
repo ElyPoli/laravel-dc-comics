ef78a91c1f6214cf93780a591fb13f0458051082
ElyPoli <itsos.poli@gmail.com>
2023-10-04 17:59:42 +0200

    added all route for movies page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ComicController;
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/comics');
 
+/**
+ * Comics page
+ */
+
 // CREATE
 Route::get("/comics/create", [ComicController::class, "create"])->name("comics.create"); // ritorna una pagina con form
 Route::post("/comics", [ComicController::class, "store"])->name("comics.store"); // leggo i dati e aggiungo il nuovo elemento alla tabella del db
@@ -30,3 +35,21 @@ Route::put("/comics/{comic}", [ComicController::class, "update"])->name("comics.
 
 // DESTROY
 Route::delete("/comics/{comic}", [ComicController::class, "destroy"])->name("comics.destroy"); // elimino l'elemento selezionato
+
+/**
+ * Movies page
+ */
+// CREATE
+Route::get("/movies/create", [MovieController::class, "create"])->name("movies.create"); // ritorna una pagina con form
+Route::post("/movies", [MovieController::class, "store"])->name("movies.store"); // leggo i dati e aggiungo il nuovo elemento alla tabella del db
+
+// READ
+Route::get("/movies", [MovieController::class, "index"])->name("movies.index"); // recupero e poi mostro tutti i dati dalla relativa tabella del db
+Route::get("/movies/{movie}", [MovieController::class, "show"])->name("movies.show"); // mostro i dettagli di un elemento specifico della relativa tabella del db
+
+// UPDATE
+Route::get("/movies/{movie}/edit", [MovieController::class, "edit"])->name("movies.edit"); // ritorna una pagina con form per modificare l'elemento
+Route::put("/movies/{movie}", [MovieController::class, "update"])->name("movies.update"); // leggo i dati e modifico l'elemento nella tabella del db
+
+// DESTROY
+Route::delete("/movies/{movie}", [MovieController::class, "destroy"])->name("movies.destroy"); // elimino l'elemento selezionato
