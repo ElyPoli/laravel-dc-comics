02ffa77948529a80f0cb36ac743225719ffa36fe
ElyPoli <itsos.poli@gmail.com>
2023-10-04 18:00:34 +0200

    added softdeletes and fillable (movies page)

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -4,14 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes; // importo il softdeletes
 
 class Movie extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     // Per alcuni dati specifico in che tipologia devono essere convertiti
     protected $casts = [
         "cast" => "array",
         "genres" => "array"
+    ];
+
+    // Creo un array al cui interno indico le colonne della tabella che voglio popolare
+    protected $fillable = [
+        "title",
+        "year",
+        "cast",
+        "genres",
+        "extract",
+        "thumbnail",
     ];
 }
